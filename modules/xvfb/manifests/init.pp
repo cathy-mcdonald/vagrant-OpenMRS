2006c6cfe4858e13136613e5c65826ef56dee602
cathy-mcdonald <github.cathy_mcdonald@spamgourmet.com>
2014-02-12 17:23:19 +1100

    Copy files to vagrant server

--- modules/xvfb/manifests/init.pp
+++ modules/xvfb/manifests/init.pp
@@ -2,4 +2,20 @@ class xvfb {
   package { 'xvfb':
     ensure => "installed",
   }
+
+  file { "/etc/init.d/xvfb":
+    ensure => present,
+    source => "puppet:///modules/xvfb/xvfb",
+    owner => "root",
+    group => "root",
+    mode => 755,
+  }
+
+  file { "/etc/profile.d/set_xvfb_display.sh":
+    ensure => present,
+    source => "puppet:///modules/xvfb/set_xvfb_display.sh",
+    owner => "root",
+    group => "root",
+    mode => 755,
+  }
 }
