fe9bb7d763ecda2493bcc0229c29285aee93d405
cathy-mcdonald <github.cathy_mcdonald@spamgourmet.com>
2014-02-16 17:39:57 +1100

    execute Xvfb command rather than trying to run as service

--- modules/xvfb/manifests/init.pp
+++ modules/xvfb/manifests/init.pp
@@ -3,19 +3,15 @@ class xvfb {
     ensure => "installed",
   }
 
-  file { "/etc/init.d/xvfb":
-    ensure => present,
-    source => "puppet:///modules/xvfb/xvfb",
-    owner => "root",
-    group => "root",
-    mode => 755,
-  }
-
-  file { "/etc/profile.d/set_xvfb_display.sh":
+  file { '/etc/profile.d/set_xvfb_display.sh':
     ensure => present,
     source => "puppet:///modules/xvfb/set_xvfb_display.sh",
     owner => "root",
     group => "root",
     mode => 755,
+  }
+
+  exec { 'xvfb':
+    command => "/usr/bin/X11/Xvfb :99 >>/home/vagrant/tmp/xvfb.out 2>&1 &",    
   }
 }
